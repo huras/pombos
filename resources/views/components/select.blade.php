9b2284020281c9b1bc17534416090504b96b841e
jean <jean@criasol>
2020-05-25 22:11:59 -0300

    Melhoria no componente select

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -13,7 +13,7 @@
             <option value="{{ (old($name)) ? old($name) : (isset($value) ? $value : 'Pesquisar ...') }}" disabled selected> {{ (old($name)) ? old($name) : (isset($value) ? $value : 'Pesquisar ...') }} </option>
             
             @foreach ($values as $value)
-                <option value='{{$value}}'> {{$value}} </option>;
+                <option value='{{$value}}' {{($value == $valueCad) ? 'selected' : $value }}> {{$value}} </option>;
             @endforeach
         </select>
     </label>
