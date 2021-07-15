54b8f8a553950ffaf59b45ec76334c41e1b1912d
Huras <hurast@gmail.com>
2021-07-14 22:04:52 -0300

    feat: listagem mobile de pombos

--- resources/views/Pombo/index.blade.php
+++ resources/views/Pombo/index.blade.php
@@ -128,7 +128,9 @@
       <span> Remover pombos selecionados  </span>
     </button>
   @endif
-  <div class='minha-table' style='    margin-top: 16px;  padding-top: 12px;  border-top: 1px solid #dee2e6; margin-bottom: 64px;'>
+
+  {{-- Versão Desktop --}}
+  <div class='minha-table desktop-only' style='margin-top: 16px;  padding-top: 12px;  border-top: 1px solid #dee2e6; margin-bottom: 64px;'>
     <table class="table table-striped datatable">    
       <thead>        
           <tr>
@@ -145,9 +147,9 @@
             <td>Pombal</td>
             <td>Cor</td>
             <td>Pai</td>
-            <td>Mãe</td>          
-            <td>Ações</td>              
-            <td style="display: none">Obs</td>              
+            <td>Mãe</td>
+            <td>Ações</td>
+            <td style="display: none">Obs</td>
           </tr>
       </thead>
       <tbody>
@@ -258,8 +260,66 @@
       </tbody>
     </table>  
   </div>
-<div>
 
+  {{-- Versão Mobile --}}
+  <div class='minha-table mobile-only' style='margin-top: 16px;  padding-top: 12px;  border-top: 1px solid #dee2e6; margin-bottom: 64px;'>
+    <table class="table table-striped datatable">    
+      <thead>        
+          <tr>
+            @if (Auth::user()->type != 0)
+              <td style="display: none">ID</td>
+              <td>  </td>
+            @endif
+            <td>Anilha</td>
+            <td>Pombal </td>
+            <td>Cor</td> 
+            
+          </tr>
+      </thead>
+      <body>
+        @foreach($pombos as $item)
+          <tr {!!$item->morto == 1 ? 'style="background-color:rgb(65,65,65); color: white;"' : '' !!} id='xaxax-{{$item->id}}' >
+            @if (Auth::user()->type != 0)
+              <td style="display: none">{{$item->id}}</td>
+              <td>                 
+                <label class="custom-checkbox">
+                  <input value='{{$item->id}}' type="checkbox" onchange="selecionaPombo({{$item->id}})"> 
+                  <span class="checkmark"></span>                  
+                </label>
+              </td>
+            @endif
+            <td>
+              <div style="display: flex; align-items: center; justify-content: space-around;">
+                <div> <a href="{{ route('pombo.profile', $item->id)}}" style='padding: 0.375rem 0.375rem;'> {{$item->anilha}} </a> </div>
+                {!!$item->morto == 1 ? '<svg version="1.1" id="Capa_1" style="height: 18px;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g>	<g>		<path fill="#FFFFFF" d="M356.233,0H155.769L94.96,139.227L163.087,512h185.826l68.127-372.773L356.233,0z M310.446,146.166L310.446,146.166			h-39.444v104.108H241V146.166h-39.444v-30.001H241V69.766h30.001v46.398h39.444V146.166z"/>	</g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>' :  ''!!}
+                <span style='display: flex; justify-content: center; align-items: center;'>
+                  @if ($item->macho == 1)
+                    <svg aria-hidden="true" style="height: 18px; margin-left: 4px;" focusable="false" data-prefix="fas" data-icon="mars" class="svg-inline--fa fa-mars fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#6cb2eb" d="M372 64h-79c-10.7 0-16 12.9-8.5 20.5l16.9 16.9-80.7 80.7c-22.2-14-48.5-22.1-76.7-22.1C64.5 160 0 224.5 0 304s64.5 144 144 144 144-64.5 144-144c0-28.2-8.1-54.5-22.1-76.7l80.7-80.7 16.9 16.9c7.6 7.6 20.5 2.2 20.5-8.5V76c0-6.6-5.4-12-12-12zM144 384c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z"></path></svg>      
+                  @elseif($item->macho == 0)                       
+                    <svg aria-hidden="true" style="height: 18px; margin-left: 4px;" focusable="false" data-prefix="fas" data-icon="venus" class="svg-inline--fa fa-venus fa-w-9" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 288 512"><path fill="#f66d9b" d="M288 176c0-79.5-64.5-144-144-144S0 96.5 0 176c0 68.5 47.9 125.9 112 140.4V368H76c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h36v36c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12v-36h36c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-36v-51.6c64.1-14.5 112-71.9 112-140.4zm-224 0c0-44.1 35.9-80 80-80s80 35.9 80 80-35.9 80-80 80-80-35.9-80-80z"></path></svg>                  
+                  @endif
+                </span>
+              </div>
+            </td>
+            <td>
+              <div style="display: flex; align-items: center; justify-content: center; flex-direction: column;">
+                <div>
+                  {{$item->pombal}}
+                </div>
+              </div>
+            </td>
+            <td>                              
+              {{$item->cor}}                
+            </td>
+          
+            
+          </tr>
+          @endforeach
+      </body>
+    </table>
+  </div>
+
+<div>
 
   <script>
     var table = undefined;
